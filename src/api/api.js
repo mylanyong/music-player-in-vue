3b1f7ef6ec758bada2dc5000bd7b1d13e4e59ebe
fhgvtfuygvfcrydutfgtyhvgg <75429885+mylanyong@users.noreply.github.com>
2021-07-20 00:32:07 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // axios.defaults.baseURL = ' http://localhost:3000/';
-axios.defaults.baseURL = ' http://meiyun.info:3000/';
+axios.defaults.baseURL = ' https://netease-cloud-music-api-two-eta.vercel.app/';
 axios.interceptors.response.use(res => res.data, err => Promise.reject(err.response.data));
 
 export default axios;
